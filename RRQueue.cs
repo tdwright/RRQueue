94a6207b36714c31edb0661ae39b2f0f58eb5c55
Tom Wright <tom@tdwright.co.uk>
2012-03-05 12:55:29 +0000

    Added actual code. Basically porting from pastebin (http://pastebin.com/U3cq7EVH), where it was dumped for inclusion in my SO question (http://stackoverflow.com/questions/3096915/simple-round-robin-moving-average-array-in-c-sharp).

---
+++ RRQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RRQueue
+{
+    class RRQueue : Queue<double>
+    {
+
+        public int capacity;
+
+        public RRQueue(int _capacity)
+        {
+            capacity = _capacity;
+        }
+
+        public new void Enqueue(double item)
+        {
+            base.Enqueue(item);
+            if (this.Count > capacity) this.Dequeue();
+        }
+
+        public double Average
+        {
+            get
+            {
+                return this.Average();
+            }
+        }
+    }
+}
